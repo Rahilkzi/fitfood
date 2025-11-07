66b1735d0ead7987b26414bafcc694de3941c56d
Rahil <rahilkazi66@gmail.com>
2025-11-07 22:50:27 +0530

    added basic UI

--- frontend/src/components/ui/Sidebar.jsx
+++ frontend/src/components/ui/Sidebar.jsx
@@ -6,16 +6,16 @@ import { PanelLeft } from "lucide-react";
 import { useIsMobile } from "@/hooks/use-mobile";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/Button";
-import { Input } from "@/components/ui/input";
-import { Separator } from "@/components/ui/separator";
-import { Sheet, SheetContent } from "@/components/ui/sheet";
-import { Skeleton } from "@/components/ui/skeleton";
+import { Input } from "@/components/ui/Input";
+import { Separator } from "@/components/ui/Separator";
+import { Sheet, SheetContent } from "@/components/ui/Sheet";
+import { Skeleton } from "@/components/ui/Skeleton";
 import {
   Tooltip,
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
+} from "@/components/ui/Tooltip";
 
 // Constants
 const SIDEBAR_COOKIE_NAME = "sidebar:state";
