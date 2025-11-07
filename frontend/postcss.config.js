2548f2edab7b882aee16646768eccc90d4ee7b50
Rahil <44476072+Rahilkzi@users.noreply.github.com>
2025-11-07 21:11:38 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,6 @@
-  export default {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
+import tailwind from "@tailwindcss/postcss";
+import autoprefixer from "autoprefixer";
+
+export default {
+  plugins: [tailwind(), autoprefixer()],
 };
